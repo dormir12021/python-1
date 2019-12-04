22026b020f10a752fc527be8b080db9c9d4f5dca
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2019-12-03 22:17:46 -0500

    Update CALENDER.py

--- CALENDER.py
+++ CALENDER.py
@@ -2,7 +2,9 @@ year = int(input("Year:"))
 if year < 0:
   print("Invalid, don't use negative numbers.")
 else:
-  divfour = (year % 4) == 0:
-    if (year % 400) == 0:
-      if (year % 100) == 0:
-        print("{0} is a leap year")
+  divfour = (year % 4) == 0
+  if (year % 400) == 0:
+    if (year % 100) == 0:
+      print("{0} is a leap year".format(year))  
+  else:  
+      print("{0} is not a leap year".format(year))  
