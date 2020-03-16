d392af6f48b47d9dcce416660e91ab839ab9a9e5
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-03-15 20:50:06 -0400

    Delete code.py

--- Python Projects/code.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #this is for, "for loop"
-math = [int(x) for x in input().split()]
-#input().split() is just basically making the number into a list
-totalnumber = 0
-for e in math:
-  totalnumber += e 
-print(totalnumber)
