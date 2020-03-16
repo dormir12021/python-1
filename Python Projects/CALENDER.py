e8daacb7da7326fbec2a2c179711457ed1aeee17
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-03-15 20:48:58 -0400

    Delete CALENDER.py

--- Python Projects/CALENDER.py
+++
@@ -1,8 +0,0 @@
-year = int(input("Year:"))
-if year < 0:
-  print("Invalid, don't use negative numbers.")
-else:
-  divfour = (year % 4) == 0:
-    if (year % 400) == 0:
-      if (year % 100) == 0:
-        print("{0} is a leap year")
