40dc3db0dc866a3b9e44262a69e9b2d1a7f2a870
dormir12021 <62727796+dormir12021@users.noreply.github.com>
2020-09-03 09:20:45 -0300

    upgrade-calender.py

--- CALENDER.py
+++ CALENDER.py
@@ -1,10 +1,25 @@
-year = int(input("Year:"))
-if year < 0:
-  print("Invalid, don't use negative numbers.")
-else:
-  divfour = (year % 4) == 0
-  if (year % 400) == 0:
-    if (year % 100) == 0:
-      print("{0} is a leap year".format(year))  
-  else:  
-      print("{0} is not a leap year".format(year))  
+from time import sleep
+def leap_year(a):
+    if a % 400 == 0 or a % 4 == 0:
+        print(f'\033[1;32m{year} is a leap year\033[m')
+    else:
+        print(f"\033[1;34m{year} is not a leap year \033[m")
+    
+
+
+
+while True:
+    try:
+        year = int(input('Year: '))
+        print('Checking')
+        sleep(2)
+    except Exception:
+        print('\033[1;41m Invalid!\033[m')
+    else:
+        if year <= 0:
+            print("\033[1;41m Invalid, don't use negative numbers.\033[m")
+            continue
+        break
+print('Loading')
+sleep(2)
+leap_year(year)
